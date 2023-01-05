e8919bdcfdb622ce2c6ddfccea0ffb54d338ba48
lusk <lucas78654321@outlook.com>
2023-01-05 10:13:38 -0300

    add disabled on button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,13 @@
 import { ButtonContainer } from "./styles";
 import { IButtonProps } from "./types";
 
-const Button = ({ title, onClick }: IButtonProps) => {
-  return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+const Button = ({ title, onClick, isDisabled }: IButtonProps) => {
+  return (
+    isDisabled === true ?
+    <ButtonContainer onClick={onClick} >{title}</ButtonContainer>
+    :
+    <ButtonContainer onClick={onClick} disabled >{title}</ButtonContainer>
+  )
 };
 
 export default Button;
